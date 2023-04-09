4bfe581501b02c69a265d8d8476a255f64ccd3ad
itsmehecker <90813931+itsmehecker@users.noreply.github.com>
2023-04-09 20:41:28 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,55 +1,28 @@
 <?php
-//You shoud add var for your server request , api key, text, warning and timeout for pop up
-$server = "http://192.168.192.164:8096";
-$Text='Hello%20World';
-$Header='warning';
-$TimeoutMs='10000';
-$api_key='56faaca8d7304e79941e700f2e9cce5f';
-s
-//function for send cUrl Post
-function cUrlGetData($url, $post_fields = null, $headers = null) {
+// Emby server details
+$server_url = "http://202.164.138.91:8096/";
+$api_key = "56faaca8d7304e79941e700f2e9cce5f";
 
-    $ch = curl_init();
-    $timeout = 5;
-    curl_setopt($ch, CURLOPT_URL, $url);
+// Create a new event listener
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $server_url . "/emby/Subscriptions/Events?api_key=" . $api_key);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-type: application/json"));
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, '{"MessageType": "LibraryChanged"}');
 
-    if (!empty($post_fields)) {
-
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields);
+// Loop indefinitely, listening for events
+while (true) {
+  // Wait for an event to be received
+  $response = curl_exec($ch);
+  
+  // Process the event
+  $event = json_decode($response, true);
+  if ($event && isset($event["Data"]["Items"])) {
+    // Handle new item added event
+    foreach ($event["Data"]["Items"] as $item) {
+      echo "New item added: " . $item["Name"] . "\n";
     }
-
-    if (!empty($headers))
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-    $data = curl_exec($ch);
-
-    if (curl_errno($ch)) {
-
-        echo 'Error:' . curl_error($ch);
-    }
-
-    curl_close($ch);
-    return $data;
+  }
 }
-
-//recieve webhooks get session_id
-$data = $_POST['data'];
-$unescaped_data = stripslashes($data);
-$obj = json_decode($unescaped_data);
-$id = $obj->Session->Id;
-
-//generate post for action playback.start
-if  ($obj->Event == "playback.start") {
-
-$url = "$server/emby/Sessions/$id/Message?Text=$Text&Header=$Header&TimeoutMs=$TimeoutMs&api_key=$api_key";
-$post_fields = "Text=$Text&Header=$Header&TimeoutMs=$TimeoutMs&api_key=$api_key";
-$headers = ["accept: */*"];
-$dat = cUrlGetData($url, $post_fields, $headers);
-
-}
-?>
