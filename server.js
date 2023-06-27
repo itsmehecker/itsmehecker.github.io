b1b3fc5417ab50ef9c6e2c1d6da4f9113539783f
itsmehecker <vaishnavsandeep.8c.svn@gmail.com>
2023-06-27 19:04:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ wss.on("connection", (ws) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 443;
 server.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
